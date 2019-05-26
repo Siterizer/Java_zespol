157fb24247fba383c096eb879b510cf37663448e
Krzosa <krzosa.karol@gmail.com>
2019-05-26 10:57:28 +0200

    testy. 8 zrobionych

---
+++ Automatbiletowy/src/test/java/machineTests.java
@@ -0,0 +1,100 @@
+import Automat.Machine;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class machineTests {
+    //private function from machine - testing
+    private static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+
+
+    @Test
+    public void returnsCorrectStateWhenEmpty(){
+        Machine testMachine = new Machine();
+
+        //no money so returns true that there is no money
+        assertTrue(testMachine.addMoney(0));
+    }
+
+    @Test
+    public void returnsCorrectStateWhenNotEmpty(){
+        Machine testMachine = new Machine();
+        testMachine.addTicket(1);
+
+        assertFalse(testMachine.addMoney(0));
+    }
+
+    @Test
+    public void addTicketTestingForIndexZeroFirst(){
+        Machine testMachine = new Machine();
+        testMachine.addTicket(0);
+        assertEquals(1.7, testMachine.getCostOfTickets());
+        testMachine.addTicket(1);
+        assertEquals(4, testMachine.getCostOfTickets());
+        testMachine.addTicket(2);
+        assertEquals(7, testMachine.getCostOfTickets());
+        testMachine.addTicket(3);
+        assertEquals(10.4, testMachine.getCostOfTickets());
+
+    }
+
+    @Test
+    public void addTicketTestingForIndexOneXFour(){
+        Machine testMachine = new Machine();
+        testMachine.addTicket(1);
+        testMachine.addTicket(1);
+        testMachine.addTicket(1);
+        testMachine.addTicket(1);
+
+        assertEquals(2.3*4, testMachine.getCostOfTickets());
+    }
+
+    @Test
+    public void addTicketTestingAll(){
+        Machine testMachine = new Machine();
+        for(int i = 0; i<6; i++) {
+            testMachine.addTicket(i);
+        }
+
+        assertEquals(1.7+2.3+3+3.4+4.6+6, testMachine.getCostOfTickets());
+    }
+
+    @Test
+    public void addTicketTestingForIndexThreeTimesTen(){
+        Machine testMachine = new Machine();
+        for (int i = 0;i<10;i++) {
+            testMachine.addTicket(3);
+        }
+
+        assertEquals(3.4*10, testMachine.getCostOfTickets());
+    }
+
+    @Test
+    public void addTicketTestingRound(){
+        Machine testMachine = new Machine();
+        for(int i = 0; i<3; i++){
+            testMachine.addTicket(4);
+            testMachine.addTicket(5);
+        }
+
+        //4.6*3+6*3 return 31.7999... we want for it to be rounded so 31.8
+        assertEquals(31.8, testMachine.getCostOfTickets());
+    }
+
+    @Test
+    public void addTicketTestingBiggerNumberThanNormal(){
+        Machine testMachine = new Machine();
+        for(int i=0;i<100;i++){
+            testMachine.addTicket(5);}
+
+        assertEquals(6*100, testMachine.getCostOfTickets());
+    }
+}
